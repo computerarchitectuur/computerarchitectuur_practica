66a980ba25872e5732137ed1e354c9ec319ceb5c
Bart Coppens <kde@bartcoppens.be>
2014-03-11 15:21:32 +0100

    Nieuwe opgave 2014 labo 1

--- pract01/jacobsthal.c
+++ pract01/jacobsthal.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <time.h>
 
-int lucas(int n) {
+int jacobsthal(int n) {
   int result = 0;
 
   if (n < 2)
-    result = 2;
+    result = n;
   else
   {
-    int n_minus_one = lucas(n-1);
-    int n_minus_two = lucas(n-2);
+    int n_minus_one = jacobsthal(n-1);
+    int n_minus_two = jacobsthal(n-2);
 
     result =  2*n_minus_two + n_minus_one;
   }
@@ -21,9 +20,9 @@ int main(int argc, char *argv[]) {
   if (argc==3) {
     int i, n=atoi(argv[1]), f, t, aantal=atoi(argv[2]);
     for (i=0; i<aantal; i++) {
-      f = lucas(n);
+      f = jacobsthal(n);
     }
-    printf("Lucas(%d) = %d \n", n, f);
+    printf("Jacobsthal(%d) = %d \n", n, f);
     return 0;
   }
   else {
