474cf61c66e229c5b2cd0ba9f4fd34f252f70ccd
Bart Coppens <kde@bartcoppens.be>
2015-03-03 17:24:23 +0100

    New function for 2015

---
+++ pract01/perrin.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int perrin(int n) {
+  int result = 0;
+
+  if (n == 0)
+    result = 3;
+  else if (n == 1)
+    result = 0;
+  else if (n == 2)
+    result = 2;
+  else
+  {
+    int n_minus_two = perrin(n-2);
+    int n_minus_three = perrin(n-3);
+
+    result =  n_minus_two + n_minus_three;
+  }
+
+  return result;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc==3) {
+    int i, n=atoi(argv[1]), f, t, aantal=atoi(argv[2]);
+    for (i=0; i<aantal; i++) {
+      f = perrin(n);
+    }
+    printf("perrin(%d) = %d \n", n, f);
+    return 0;
+  }
+  else {
+    printf("Formaat: %s waarde aantal_iteraties\n", argv[0]);
+    return 1;
+  }
+}
