7e5ef6dea06d878243d4c7b4a58a286a37229162
babrath <bert.abrath@UGent.be>
2018-05-19 13:40:38 +0200

    Updated answer tool.

--- pract08/AnswerQuestions.py
+++ pract08/AnswerQuestions.py
@@ -10,12 +10,24 @@ def decode_address_range(answer):
     end = int(tokens[1])
     return (start, end)
 
-def ask_cache_contents(iteration):
+def ask_cache_contents_part1(iteration):
     contents = []
     print('Geef de inhoud van de cache op iteratie: ' + str(iteration))
     for iii in range(0, 4):
         answer = input('Geef het bereik van adressen aanwezig in blok ' + str(iii) + ' in de vorm BEGINADRES-EINDADRES (eindadres inclusief): ')
         contents.append(decode_address_range(answer))
+
+    return contents
+
+def ask_cache_contents_part2(iteration):
+    contents = []
+    print('Geef de inhoud van de cache op iteratie: ' + str(iteration))
+    for iii in range(0, 2):
+        blocks = []
+        for jjj in range(0, 4):
+            answer = input('Geef het bereik van adressen aanwezig in set ' + str(iii) + ' blok ' + str(jjj) + ' in de vorm BEGINADRES-EINDADRES (eindadres inclusief): ')
+            blocks.append(decode_address_range(answer))
+        contents.append(blocks)
 
     return contents
 
@@ -34,15 +46,15 @@ if __name__ == '__main__':
     results['group_id'] = int(input("Geef uw groepsnummer in: "))
 
     # Get the answers for the actual questions
-    ask_question(answers, 'Dubbele lus 1a', ask_cache_contents, 10)
-    ask_question(answers, 'Dubbele lus 1b', ask_cache_contents, 20)
-    ask_question(answers, 'Dubbele lus 1c', ask_cache_contents, 50)
+    ask_question(answers, 'Dubbele lus 1a', ask_cache_contents_part1, 10)
+    ask_question(answers, 'Dubbele lus 1b', ask_cache_contents_part1, 20)
+    ask_question(answers, 'Dubbele lus 1c', ask_cache_contents_part1, 50)
 
-    ask_question(answers, 'Matrixvermenigvuldiging 1c', ask_cache_contents, 50)
+    ask_question(answers, 'Matrixvermenigvuldiging 1c', ask_cache_contents_part2, 50)
 
-    ask_question(answers, 'Matrixvermenigvuldiging 3a', ask_cache_contents, 10)
-    ask_question(answers, 'Matrixvermenigvuldiging 3b', ask_cache_contents, 20)
-    ask_question(answers, 'Matrixvermenigvuldiging 3c', ask_cache_contents, 50)
+    ask_question(answers, 'Matrixvermenigvuldiging 3a', ask_cache_contents_part2, 10)
+    ask_question(answers, 'Matrixvermenigvuldiging 3b', ask_cache_contents_part2, 20)
+    ask_question(answers, 'Matrixvermenigvuldiging 3c', ask_cache_contents_part2, 50)
 
     # Dump the results as a JSON file
     with open('practicum8_resultaten.json', 'w') as f_res:
